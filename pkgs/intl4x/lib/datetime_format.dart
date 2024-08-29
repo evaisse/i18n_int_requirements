33dfbecf1575ce45ad643c27f19005335bbe30e5
Moritz <mosum@google.com>
2024-08-29 13:59:02 +0200

    Add extensions on `DateTime` and `List`

--- pkgs/intl4x/lib/datetime_format.dart
+++ pkgs/intl4x/lib/datetime_format.dart
@@ -2,6 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'intl4x.dart';
+
 export 'src/datetime_format/datetime_format.dart';
 export 'src/datetime_format/datetime_format_options.dart';
 export 'src/options.dart';
+
+extension DatetimeFormatIntl4x on DateTime {
+  String toLocaleDateString([Locale? locale]) =>
+      Intl(locale: locale).datetimeFormat().format(this);
+}
